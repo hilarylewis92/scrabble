e535abb4eb28c1895ea3f10e9ec2eb02fc694b7a
Hilary Lewis <hilarylewis92@gmail.com>
2016-11-08 15:15:49 -0700

    Webpack starter kit

---
+++ test/score-words-test.js
@@ -0,0 +1,8 @@
+var assert = require('chai').assert;
+const ScoreWord = require ('../lib/score-words.js');
+
+describe('Score word test', function() {
+  it('should be a function', function(){
+    assert(true);
+  })
+})
